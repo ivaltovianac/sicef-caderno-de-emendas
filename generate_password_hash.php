108e5eeba794faa12daf40a9ec8f65cc267c1513
ivaltovianac <ivaltovianna@gmail.com>
2025-08-18 15:24:44 -0300

    remoção de arquivos

--- generate_password_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Ferramenta simples para gerar hash BCRYPT
-header('Content-Type: text/plain; charset=utf-8');
-$pwd = $_GET['p'] ?? 'password1';
-echo password_hash($pwd, PASSWORD_BCRYPT);
